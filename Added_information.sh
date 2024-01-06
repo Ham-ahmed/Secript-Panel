6b0efbcb8e0c970f23722931410372b46a103897
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-06 19:50:32 +0200

    Add files via upload

---
+++ Added_information.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+echo ">>>H-Ahmed-Panel<<<"
+echo " "
+echo " "
+sleep 1s
+echo "7-1-2023 Added"
+
+echo "> panel
+> novalerstore ..... has been added "
+sleep 4s
+echo "> plugins
+> subssupport........updated "
+> permanentclockt....has been added "
+> NewVirtualKeyBoard_sub.....has been added "
+sleep 4s
+echo "> Tools
+> novalerstore ..... has been added "
+> service_app........has been added "
+sleep 4s
+echo "> skin premium fhd updated
+> premium-fhd-black_2.5.has been added "
+> premium-fhd-blue_2.5..has been added "
+sleep 4s
+
+
+echo ">>> Upload HAhmed 6-1-2023<<<"
+exit 0
