ee0583b52e2b0c4832a9f6840638c4f1998db906
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-06 20:16:58 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -8,17 +8,17 @@ echo "7-1-2023 Added"
 echo "> panel
 > novalerstore ..... has been added "
 sleep 4s
-echo "> plugins
-> subssupport........updated "
-> permanentclockt....updated "
-> NewVirtualKeyBoard_sub....updated "
+echo "> plugins Added
+> subssupport
+> permanentclockt
+> NewVirtualKeyBoard_sub
 sleep 4s
 echo "> Tools
 > service_app........updated "
 sleep 4s
 echo "> skin premium-fhd updated
-> premium-fhd-black_2.5..updated "
-> premium-fhd-blue_2.5..updated "
+> premium-fhd-black_2.5
+> premium-fhd-blue_2.5
 sleep 4s
 
 echo ">>> upload..7-1-2023 <<<"
