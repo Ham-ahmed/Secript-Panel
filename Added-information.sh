0d11dc935adb9d2fee684e3c38aa5dd780067e7c
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-06 19:25:48 +0200

    Add files via upload

---
+++ Added-information.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo ">>>Hamdy Ahmed-Panel<<<"
+echo " "
+echo " "
+sleep 1s
+echo "7-1-2023
+ Added"
+
+echo "> panel
+> novalerstore ..... has been added "
+sleep 4s
+> subssupport1.5.8r9........updated "
+> permanentclockt....has been added "
+> service_app........has been added "
+sleep 4s
+echo "> skin premium fhd 2.5 Add
+> skins-premiumfhd-black_2.5.has been added "
+> skins-premiumfhd-blue_2.5..has been added "
+> NewVirtualKeyBoard_sub.....has been added "
+sleep 4s
+
+
+echo ">>> Upload HAhmed <<<"
+exit 0
