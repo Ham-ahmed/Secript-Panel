ad49522fee08cbb68cf6d6dd285f2ee195da8cdd
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-06 20:52:51 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -1,97 +1,26 @@
 #!/bin/sh
-echo ">>>eliesat scripts 434/1000<<<"
+echo ">>>H-Ahmed-Panel<<<"
 echo " "
 echo " "
 sleep 1s
-echo "25-12-2023
- تم اضافة"
-
-echo "> settings-channels... 10 channels lists"
-sleep 3s
+echo "7-1-2023
+ Added"
 
 
-echo "24-12-2023"
+echo "> skin premium fhd 2.5 updated
+> skin premium fhd blue 2.5 updated
+> skin premium fhd black 2.5 updated"
 sleep 3s
-echo "> skin premium fhd 2.3 updated
-> skin premium fhd blue 2.3 updated
-> skin premium fhd black 2.3 updated"
+echo "> panel  updated"
+echo "> novalerstore  updated"
 sleep 3s
-echo "> softcams ncams 14.3  updated"
-echo "> softcams oscams 11727r799 mohamed os  updated"
-sleep 3s
-echo "> skins-openpli-9.x-......"
-sleep 3s
-
-echo "23-12-2023"
-sleep 3s
-echo "> skins-openpli-9.x-......"
+echo "> plugins+Tools"
+echo "> novalerstore  updated"
+echo "> subssupport  updated"
+echo "> permanentclockt  updated"
+echo "> NewVirtualKeyBoard_sub  updated"
+echo "> service_app  updated"
 sleep 3s
 
-echo "22-12-2023"
-sleep 3s
-echo "> skins-openpli-9.x-......"
-sleep 3s
-
-echo "> skin premium fhd updated"
-sleep 3s
-
-echo "> skin premium fhd black updated"
-sleep 3s
-
-echo "> skin premium fhd blue updated"
-sleep 3s
-
-
-echo "04-12-2023"
-sleep 3s
-
-echo "> #remove-emus"
-sleep 3s
-
-echo "> #remove-playlists-ipaudio"
-sleep 3s
-
-echo "> #remove-playlists-iptosat"
-sleep 3s
-
-echo "> #remove-playlists-tsiplayer"
-sleep 3s
-
-echo "02-12-2023"
-sleep 3s
-
-echo "> #remove-emus-config-files"
-sleep 3s
-
-echo "> #remove-epg-rytec-sources"
-sleep 3s
-
-echo "> #remove-epg-ziko-sources-config"
-sleep 3s
-
-echo "> #remove-picons"
-sleep 3s
-
-echo "> #remove-scripts"
-sleep 3s
-
-echo "01-12-2023"
-sleep 3s
-
-echo "> extensions-e2iplayer-tsiplayer-mod-linuxsat-23.11.2023 updated"
-sleep 3s
-
-echo "> #remove-bootlogo-swapper-logos"
-sleep 3s
-
-echo "> #remove-crash-logs"
-sleep 3s
-
-echo "> #remove-e2iplayer"
-sleep 3s
-
-echo "> #remove-multistalker-portals"
-sleep 3s
-
-echo ">>> Eliesat enjoy <<<"
+echo ">>> Uploadin 7-1-2023 <<<"
 exit 0
