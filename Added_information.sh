7faebfd868d6e4b69a723b1a0e9941f02e68d1fa
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-10 04:03:38 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -3,10 +3,21 @@ echo ">>>H-Ahmed-Panel<<<"
 echo " "
 echo " "
 sleep 1s
-echo "7-1-2024
+echo "10-1-2024
  Added"
 
 
+echo "> new nitor skin 2024  updated
+> new history zap to 1.0.35 updated
+> Sherlockmod plugin updated"
+sleep 3s
+echo "> oscam emu to 11731 updated"
+echo "> New-channels 10-2-2024 MohamedOS"
+sleep 3s
+
+
+echo "7-1-2024"
+sleep 3s
 echo "> skin premium fhd 2.5 updated
 > skin premium fhd blue 2.5 updated
 > skin premium fhd black 2.5 updated"
