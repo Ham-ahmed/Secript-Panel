dfe3ea1857c600e8002d207fcf845eac2f748d28
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-12 19:28:28 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -9,7 +9,11 @@ echo "12-1-2024
 
 echo "12-1-2024"
 sleep 3s
+echo "> skin Bohlala update
+echo "> skin kill pro update"
 echo "> New channels 10-1-2024 updated
+echo "> skin ozeta fhd update
+echo "> fix ipk package update
 sleep 3s
 
 
