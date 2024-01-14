78ae4b65c6c08b187a3600cc7e099f604363b795
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-15 01:22:23 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -17,7 +17,7 @@ echo "13-1-2024"
 sleep 3s
 echo "> skin premium fhd 2.6 updated
 > skin premium fhd blue 2.6 updated
-> skin premium fhd black 2.6 updated"
+> skin premium fhd black 2.6 updated
 sleep 3s
 
 
