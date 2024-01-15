a78a31097660c41682250caa9bfe0606f00a2bd6
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-15 23:02:23 +0200

    Add files via upload

--- ipaudiopro_picons_mnasr_140124.sh
+++ ipaudiopro_picons_mnasr_140124.sh
@@ -1,54 +1,13 @@
 #!/bin/sh
 #
-cd /tmp
-set -e 
- wget "https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel/main/ipaudiopro_picons_mnasr_140124.tar.gz"
+
+wget -O /var/volatile/tmp/enigma2-ipaudiopro-picons-mnasr-140124_v1.0_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel/main/enigma2-ipaudiopro-picons-mnasr-140124_v1.0_all.ipk"
 wait
-tar -xzf ipaudiopro_picons_mnasr_140124.tar.gz  -C /
+opkg install --force-overwrite /tmp/*.ipk
 wait
-cd ..
-set +e
-rm -f /tmp/ipaudiopro_picons_mnasr_140124.tar.gz
+rm -f /var/volatile/tmp/enigma2-ipaudiopro-picons-mnasr-140124_v1.0_all.ipk
 sleep 2;
-echo "" 
-echo "" 
-echo "**************************************************************************************************"
-echo "#  INSTALLED SUCCESSFULLY #"
-echo "*      888888  888888    .d88b.    88888     88    88   .d88b.  *"
-echo "*      88  88  88  88   88    88   88   88   88    88  88    88 *"
-echo "*      88    88    88  88      88  88    88  88oooo88  88oooo88 *"
-echo "*      88          88   88    88   88    88  88    88  88    88 *"
-echo "*      88          88   .d88b.     888888    88    88  88    88 *"
-echo "*          EDITE - Hamdy Ahmed       Novaler 4k Egypt *"
-echo "*           888888  88    88  oo  8888     88  8888 *"
-echo "*           88      88  88    88  88 88    88  88   *"
-echo "*           88888   8888      88  88  88   88  8888 *"
-echo "*              88   88  88    88  88    88 88    88 *"
-echo "*           88888   88   88   88  88     8888  8888 *"
-echo "*                Enigma2 restart is required            *"
-echo "*************************************************************************************************"
-echo "   UPLOADED BY  >>>>   HAMDY_AHMED "
-sleep 4;
-	echo '========================================================================================='
-###########################################                                                                                                                  
-echo ". >>>>         RESTARING     <<<<"
-echo "*************************************************************************************************"
-wait
-killall -9 enigma2
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
