c38bb88884a8877f1b160a5d072002e5155a892d
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-17 00:07:38 +0200

    Update enigma2-plugin-extensions-ipaudiopro_1.1.sh

--- enigma2-plugin-extensions-ipaudiopro_1.1.sh
+++ enigma2-plugin-extensions-ipaudiopro_1.1.sh
@@ -3,7 +3,7 @@
 
 wget -O /var/volatile/tmp/enigma2-plugin-extensions-ipaudiopro_1.1.tar.gz "https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel/main/enigma2-plugin-extensions-ipaudiopro_1.1.tar.gz"
 wait
-opkg install --force-overwrite /tmp/*.ipk
+tar -xzf enigma2-plugin-extensions-ipaudiopro_1.1.tar.gz  -C /
 wait
 rm -f /var/volatile/tmp/enigma2-plugin-extensions-ipaudiopro_1.1.tar.gz
 sleep 2;
