1da27887ac0cf94ce4c2b398dbb8c903c83fbb48
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-16 14:23:25 +0200

    Add files via upload

---
+++ biko-IPAudioPro-json.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "> Downloading & installing IPAudioPro.json file  Please Wait ..."
+sleep 3s
+dir=/etc/enigma2
+
+wget -O $dir/etc/enigma2/IPAudioPro.json https://raw.githubusercontent.com/biko-73/ipaudio/main/IPAudioPro.json
+
+
+if [ -d $dir/IPAudioPro.json ]; then
+cp $dir/IPAudioPro.json $dir/IPAudioPro.json/
+fi
+
+echo "> installation of IPAudioPro.json file  finished"
+sleep 3s
+
+exit 0
