1fc940c1a1af89803159d7f885798dd3e088a43a
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-17 06:03:31 +0200

    Add files via upload

---
+++ channels_backup_AHMED-HUSSEIN.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+##
+channel=AHMED-HUSSEIN
+version=motor
+
+echo "> Downloading "$channel" "$version" Channels List  Please Wait ......"
+sleep 3s
+
+wget -O /var/volatile/tmp/$channel-$"version".tar.gz "https://gitlab.com/h-ahmed/Panel/-/raw/main/channels_backup_AHMED-HUSSEIN.tar.gz"
+
+echo ""
+
+
+rm -rf /etc/enigma2/lamedb
+rm -rf /etc/enigma2/*list
+rm -rf /etc/enigma2/*.tv
+rm -rf /etc/enigma2/*.radio
+rm -rf /etc/tuxbox/*.xml
+
+
+cd /tmp
+set -e
+
+echo "> Installing  "$channel" "$version" Channels List Please Wait ......"
+sleep 3s
+echo
+tar -xvf $channel-$"version".tar.gz -C /
+set +e
+rm -f $channel-$"version".tar.gz
+echo
+echo "   >>>>   Reloading Services - Please Wait   <<<<"
+wget -qO - http://127.0.0.1/web/servicelistreload?mode=0 > /dev/null 2>&1
+sleep 2
+echo
+init 3
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
