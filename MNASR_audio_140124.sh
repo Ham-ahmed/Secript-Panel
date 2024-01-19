275808f2dd3f069fa2dcafa8b11e7e90c9424f6a
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-19 02:52:58 +0200

    Add files via upload

--- MNASR_audio_140124.sh
+++ MNASR_audio_140124.sh
@@ -1,44 +1,31 @@
-#!/bin/sh
-#
+#!/bin/bash
 
-wget -O /etc/enigma2/IPAudioPro.json "https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel/main/IPAudioPro.json"
-wait
-sleep 2;
-echo "   UPLOADED BY  >>>>   H-Ahmed "
-sleep 4;
-	echo '========================================================================================================================='
-###########################################                                                                                                                  
-echo ". >>>>         RESTARING     <<<<"
-echo "**********************************************************************************"
-wait
-killall -9 enigma2
+
+#config
+plugin=ipaudio
+version=pro
+url=https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel/main/MNASR_audio_140124.tar.gz
+package=/var/volatile/tmp/$plugin-$version.tar.gz
+
+#download & install
+echo "> Downloading $plugin-$version package  please wait ..."
+sleep 3s
+
+wget -O $package --no-check-certificate $url
+tar -xzf $package -C /
+extract=$?
+rm -rf $package >/dev/null 2>&1
+
+echo ''
+if [ $extract -eq 0 ]; then
+echo "> $plugin-$version package installed successfully"
+echo "> By H-Ahmed"
+sleep 3s
+
+else
+
+echo "> $plugin-$version package installation failed"
+sleep 3s
+fi
+
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
