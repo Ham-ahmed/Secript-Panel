f9abed808d04583aaf05adc38cb5e57a40da9391
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-19 20:40:08 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -14,7 +14,7 @@ sleep 3s
 
 echo "17-01-2024"
 sleep 3s
-echo "> ipaudiopro v1.1 plugin updated
+> ipaudiopro v1.1 plugin updated
 > Channel by Mohammed Nasr 2024 updated
 > Channel by Ahmed hussin 2024 motor updated
 > Channel by Mostafa Gh 2024 motor updated
