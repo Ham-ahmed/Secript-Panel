8ae4ed24e151446e47e1b4d2ca3f6b68da505d23
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-19 02:54:42 +0200

    Add files via upload

---
+++ picons-ipaudio_MNasr.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+#config
+plugin=picons
+version=ipaudio
+url=https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel/main/picons-ipaudio_MNasr.tar.gz
+package=/var/volatile/tmp/$plugin-$version.tar.gz
+
+#download & install
+echo "> Downloading $plugin-$version package  please wait ..."
+sleep 3s
+
+wget -O $package --no-check-certificate $url
+tar -xzf $package -C /
+extract=$?
+rm -rf $package >/dev/null 2>&1
+
+echo ''
+if [ $extract -eq 0 ]; then
+echo "> $plugin-$version package installed successfully"
+echo "> By H-Ahmed"
+sleep 3s
+
+else
+
+echo "> $plugin-$version package installation failed"
+sleep 3s
+fi
+
+exit 0
