dbe607e3809f995da183e2ec870fed53c2e99a71
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-21 12:41:21 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -9,7 +9,8 @@ echo "21-1-2024
 
 echo "21-01-2023"
 sleep 3s
-echo "> Estuaryfhd spa v2 edite Mod HAhmed updated
+echo "> Estuaryfhd spa v2 It has been fixed updated
+> Eustaryfhd spa v3 new mod A Ahmed updated
 sleep 3s
 
 
