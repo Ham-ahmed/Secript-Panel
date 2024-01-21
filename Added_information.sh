ed47ca10b14a9febc4065149e864c948f87c5365
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-21 12:14:12 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -9,8 +9,8 @@ echo "21-1-2024
 
 echo "21-1-2024"
 sleep 3s
-echo "> Estuaryfhd v3 Mod Hamdy Ahmed updated
-echo > Estuaryfhd v2 Edite Mod Hamdy Ahmed updated
+echo "> Estuaryfhd v3 Mod H Ahmed updated
+echo > Estuaryfhd v2 Edite Mod H Ahmed updated
 sleep 3s
 
 
