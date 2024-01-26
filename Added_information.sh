ff45840b5a74ba5d87148766cb1732cf0bbd2831
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-26 18:37:32 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -9,7 +9,7 @@ echo "26-1-2024
 
 echo "26-1-2024"
 sleep 3s
-echo "> Mx GrayDotsfhd vip xtra H Ahmed updated
+> Mx GrayDotsfhd vip xtra H Ahmed updated
 > Mx priofhd xtra mod H Ahmed updated
 sleep 3s
 
