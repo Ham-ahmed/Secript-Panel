25ccff62ea0ed9034a69d3dda8e63fe2792043c5
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-28 14:47:31 +0200

    Add files via upload

---
+++ New-update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#config
+espp=$(cat /etc/enigma2/settings | grep config.plugins.AJPanel.backupPath | cut -d '=' -f 2)
+
+
+pack="ajpanel_menu.xml"
+package=$espp$pack
+url=https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel/main/ajpanel_menu_HA.xml
+if [ "$espp" == "/media/hdd/AJPanel_Backup/" ]; then
+pack="ajpanel_menu_Elie.xml"
+package=$espp$pack
+fi
+
+
+echo "> done"
+sleep 3s
+exit 0
