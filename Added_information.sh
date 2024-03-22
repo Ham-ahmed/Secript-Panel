6964ab11bf7faa9e1f51ac06cfe4d2f4508f22c3
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-22 22:52:36 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -7,20 +7,9 @@ echo "23-3-2024
  Added"
 
 
-echo "23-3-2024"
-sleep 3s
-echo "> oscam emu 11748 r800 updated 
-      >powercam oscam 11749 r800 update 
-      > supcam oscam 11749 r800 update
-      > ultaracam oscam 11749 r800 update
-      > skins aglare fhd 2.3 Atv Egami Spa update
-      > skins aglare fhd 2.3 openpli update
-sleep 3S
-
-
 echo "19-3-2024"
 sleep 3s
-      > plugin extensions ajpanel_v9.3.0_all updated
+echo "> plugin extensions ajpanel_v9.3.0_all updated
       > plugin extensions ajpanel_v9.2.0_all update 
       > plugin extensions ajpanel_v9.1.0_all update
       > plugin extensions ajpanel_v9.0.0_all update
