957c6635228d3332bfc2474a090e7ada274a5193
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-22 22:44:02 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -9,7 +9,8 @@ echo "23-3-2024
 
 echo "23-3-2024"
 sleep 3s
-      echo "> powercam oscam 11749 r800 update 
+      echo "> oscam emu 11748 r800 updated 
+      >powercam oscam 11749 r800 update 
       > supcam oscam 11749 r800 update
       > ultaracam oscam 11749 r800 update
       > skins aglare fhd 2.3 Atv Egami Spa update
