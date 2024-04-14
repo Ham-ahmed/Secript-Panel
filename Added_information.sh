a6c0c5957438855118c33bc14937cdf649635064
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-04-14 18:09:52 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -3,12 +3,17 @@ echo ">>> H-Ahmed-Panel v.4 <<<"
 echo " "
 echo " "
 sleep 1s
-echo "13-4-2024
+echo "14-4-2024
  Added"
+
+echo "14-4-2024
+sleep 3s
+      > e2iplayer+TSIPlayer 14-04-2024 updated
+sleep 3S
 
 echo "13-4-2024"
 sleep 3s
-echo "> oscam-emu 11764 r801 updated
+      > oscam-emu 11764 r801 updated
       > powercam oscam 11764 r801 updated
       > supcam oscam 11764 r801 updated 
       > ultracam oscam 11764 r801 updated
@@ -360,5 +365,5 @@ echo "> NewVirtualKeyBoard_sub  updated"
 echo "> service_app  updated"
 sleep 3s
 
-echo ">>> Last Updatting 13-4-2024 <<<"
+echo ">>> Last Updatting 14-4-2024 <<<"
 exit 0
