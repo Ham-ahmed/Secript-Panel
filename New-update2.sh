e36b6f71dff1e0c5e8b7f0826e3cdecaeebed40b
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-06-26 09:35:25 +0300

    Update New-update2.sh

--- New-update2.sh
+++ New-update2.sh
@@ -2,21 +2,6 @@
 #config
 espp=$(cat /etc/enigma2/settings | grep config.plugins.AJPanel.backupPath | cut -d '=' -f 2)
 
-
-pack="ajpanel_menu.xml"
-package=$espp$pack
-url=https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel/main/ajpanel_menu_HA2.xml
-if [ "$espp" == "/media/hdd/AJPanel_Backup/" ]; then
-pack="ajpanel_menu_HA2.xml"
-package=$espp$pack
-fi
-
-#download & install
-echo "> installing latest update please wait ..."
-sleep 3s
-wget -O $package --no-check-certificate $url
-
-
 echo "> done"
 sleep 3s
 exit 0
