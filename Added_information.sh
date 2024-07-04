e7aa71311b577ab5996cbda4fe8e1ef747e8ccf5
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-07-04 15:32:22 +0300

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -1,103 +1,124 @@
 #!/bin/sh
 DATE="$(date +%a.%d.%b.%Y)"
-echo ">>>H-Ahmedpanel $DATE<<<"
+echo ">>>H-Ahmed_panelv5 $DATE<<<"
 echo " "
 sleep 1s
 echo "> 04-07-2024"
-sleep 3s
+sleep 2s
 
 echo " "
 echo "> 4-7-2024"
-sleep 3s
+sleep 2s
 
 echo "> Xklass plugin v.016 kiddac 4-7-2024 updated"
+sleep 2s
+
+echo " "
+echo "> 4-7-2024"
+sleep 2s
+
+echo "> levi45 addon manager 10.1-r19 4-7-2024 updated"
+sleep 2s
+
+echo " "
+echo "> 4-7-2024"
+sleep 2s
+
+echo "> levi45 multicammanager 10.1-r22 4-7-2024 updated"
 sleep 3s
 
 echo " "
 echo "> 4-7-2024"
 sleep 3s
+
+echo "> Satvenus panel v7.3.3 4-7-2024 updated"
+sleep 2s
+
+echo " "
+echo "> 4-7-2024"
+sleep 2s
 
 echo "> Channels-MohamdOS Motor 1-7-2024 updated"
-sleep 3s
+sleep 2s
 
 echo " "
 echo "> 4-7-2024"
-sleep 3s
+sleep 2s
 echo " "
 echo "> 4-7-2024"
-sleep 3s
+sleep 2s
 
 echo "> skins aglare fhd 3.7 MNasr 4-7-2024 updated"
-sleep 3s
+sleep 2s
 
 echo " "
 echo "> 4-7-2024"
-sleep 3s
+sleep 2s
 
 echo "> novacampro py3.12_3.0-r0 1-7-2024 updated"
-sleep 3s
+sleep 2s
 
 echo " "
 echo "> 4-7-2024"
-sleep 3s
+sleep 2s
 
 echo "> novacampro py3.11_3.0-r0 1-7-2024 updated"
-sleep 3s
+sleep 2s
 
 echo " "
 echo "> 4-7-2024"
-sleep 3s
+sleep 2s
 
 echo "> novacampro py3.9_3.0-r0 1-7-2024 updated"
-sleep 3s
+sleep 2s
 
 echo "> xtraevent plugin v6.6 4-7-2024 updated"
-sleep 3s
+sleep 2s
 
 echo " "
 echo "> 4-7-2024"
-sleep 3s
+sleep 2s
 
 echo "> oscam-emu 11799 r802 updated"
-sleep 3s
+sleep 2s
 
 echo "> powercam oscam 11799 r802 updated"
-sleep 3s
+sleep 2s
 
 echo "> supcam oscam 11799 r802 updated"
-sleep 3s
+sleep 2s
 
 echo "> ultracam oscam 11799 r802 updated"
-sleep 3s
+sleep 2s
 
 echo "> gosatplus oscam 11799 r802 updated"
-sleep 3s
+sleep 2s
 
 echo " "
 echo "> 4-7-2024"
-sleep 3s
+sleep 2s
 
 echo "> xtraevent plugin v6.7 4-7-2024 updated"
-sleep 3s
+sleep 2s
 
 echo " "
 echo "> 3-7-2024"
-sleep 3s
+sleep 2s
 
 echo "> oscam-emu 11794 r802 updated"
-sleep 3s
+sleep 2s
 
 echo "> powercam oscam 11794 r802 updated"
-sleep 3s
+sleep 2s
 
 echo "> supcam oscam 11794 r802 updated"
-sleep 3s
+sleep 2s
 
 echo "> ultracam oscam 11794 r802 updated"
-sleep 3s
+sleep 2s
 
 echo "> gosatplus oscam 11794 r802 updated"
-sleep 3s
+sleep 2s
 
 echo ">>> Last Updatting 4-7-2024 <<<"
 exit 0
