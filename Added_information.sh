2139886c899314208ca2e654082fcc062e10a35e
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-07-04 05:26:10 +0300

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -30,6 +30,26 @@ sleep 3s
 echo "> skins aglare fhd 3.7 MNasr 4-7-2024 updated"
 sleep 3s
 
+echo " "
+echo "> 4-7-2024"
+sleep 3s
+
+echo "> novacampro py3.12_3.0-r0 1-7-2024 updated"
+sleep 3s
+
+echo " "
+echo "> 4-7-2024"
+sleep 3s
+
+echo "> novacampro py3.11_3.0-r0 1-7-2024 updated"
+sleep 3s
+
+echo " "
+echo "> 4-7-2024"
+sleep 3s
+
+echo "> novacampro py3.9_3.0-r0 1-7-2024 updated"
+sleep 3s
 
 echo "> xtraevent plugin v6.6 4-7-2024 updated"
 sleep 3s
