c800fe85fb2a2d7682e3be3ee0edab8f04600b84
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-08-06 14:17:22 +0300

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -11,7 +11,7 @@ echo "> 06-08-2024"
 sleep 2s
 
 echo "> Channels-MohamdOS Motor 04-8-2024 updated"
-echo "> bouquetmakerxtream plugin v.1.18 23-7-2024 updated"
+echo "> bouquetmakerxtream v.1.18 23-7-2024 updated"
 echo "> Backup build New section 6-8-2024 cearted"
 sleep 2s
 
