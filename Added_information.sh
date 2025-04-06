a3fbfa9775af68c1f891737a2c68004ce1a788fa
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-04-06 16:04:56 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -2,14 +2,19 @@
 DATE="$(date +%a.%d.%b.%Y)"
 echo ">>>H-Ahmed_panelv5 $DATE<<<"
 echo " "
-echo "> 30-03-2025"
+echo "> 06-04-2025"
 sleep 2s
 
 echo " "
-echo "> 13-03-2025"
+echo "> 06-04-2025"
 sleep 2s
-echo "> 26-xklass Manage playlists 30-03-2025 updated"
-echo "> 26-xtreamity Manage playlists 30-03-2025 updated"
+echo "> subssupport 1.7.0-R18 06-04-2025 updated"
+echo "> xtraevent v6.820 06-04-2025 updated"
+echo "> oscam emu levi45 11878 802 06-04-2025 updated"
+echo "> xklass plugin v1.44 06-04-2025 updated"
+echo "> xstreamity plugin v4.92 06-04-2025 updated"
+echo "> bouquetmakerxtream 1.47 06-04-2025 updated"
+echo "> fullhdglass17 skin 9.32 06-04-2025 updated"
 sleep 2s
 
 echo " "
@@ -207,5 +212,5 @@ echo "> ِAjpanel plugin 10.5.1 02-02-2025 updated"
 echo "> ipaudiopro plugin v.1.5 24 02-02-2025 updated"
 sleep 2s
 
-echo ">>> Last Updatting 30-03-2025 <<<"
+echo ">>> Last Updatting 06-04-2025 <<<"
 exit 0
